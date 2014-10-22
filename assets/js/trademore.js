03561cf2c07ac0a70c5adb4f2f50d9b04b1efe08
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2014-10-22 14:51:02 +0100

    Added assets from previous project

---
+++ assets/js/trademore.js
@@ -0,0 +1,16 @@
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$(document).ready(function(){
+	if(document.URL == 'http://whatplant.github.io/?submitted=true'){
+		$('#form_submitted').finish().fadeIn("fast").delay(4000).fadeOut("slow");
+	};
+});
