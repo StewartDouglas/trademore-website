6b3f2ba057a87aedac69c013d5da32444c021421
Stewart Douglas <stewart.douglas13@imperial.ac.uk>
2014-11-19 16:13:40 +0000

    Remove our address

--- assets/js/trademore.js
+++ assets/js/trademore.js
@@ -1,12 +1,16 @@
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
-    $('.page-scroll a').bind('click', function(event) {
+    $('.homepage a').on('click', function(event) {
         var $anchor = $(this);
+        //var frag = $anchor.attr('href').match(/#\w+/g)[0];
+        //console.log("frag: " + frag);
+        //console.log($anchor)
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
+
 });
 
 // Closes the Responsive Menu on Menu Item Click
